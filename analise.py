5a84dedb4e4366f80d1a57e706f56c8f209b7fcd
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-04-09 17:36:52 -0400

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import io
 
+
 df = pd.read_csv('Ecommerce_Consumer_Behavior_Analysis_Data.csv')
 column_names = df.columns
 pd.set_option('display.max_rows', None)  # mostra todas as linhas (tira os ... que o pandas resume)
@@ -45,9 +46,10 @@ pd.set_option('display.expand_frame_repr', False) # deve mostrar todos os cabeca
 
 
 def info_data():
-    print('\nDeseja ver que informação da tablea?\n1 - Quantidade de linhas e colunas\n2 - Nome das colunas\n3 - Tipos de dados contidos no arquivo\n4 - Quantidade de cada genero\n5 - Distribuição por nível de escolaridade\n6 - Distribuição por ocupação/ profissão\n7 - Localizações de compras realizadas\n8 - Tempo gasto por cliente no produto')
+    print('\nDeseja ver que informação da tablea?\n1 - Quantidade de linhas e colunas\n2 - Nome das colunas\n3 - Tipos de dados contidos no arquivo\n4 - Quantidade de cada genero\n5 - Distribuição por nível de escolaridade\n6 - Distribuição por categoria de compra\n7 - Localizações de compras realizadas\n8 - Tempo gasto por cliente no produto\n9 - Avaliação do produto por categoria\n10 - ')
 
     op = int(input('- '))
+
     if op == 1:
         print(f'\nNúmero de linhas: {df.shape[0]} | Número de colunas: {df.shape[1]}\n')
     elif op == 2:
@@ -67,6 +69,47 @@ def info_data():
             print(df['Gender'].value_counts(normalize=True) * 100)
         elif op == 'N'.lower():
             print(df['Gender'].value_counts())
+
+    elif op == 5:
+        print('Deseja ver em porcentagem?\nS/N')
+        op = input('- ')
+        if op == 'S'.lower():
+            print(df['Education_Level'].value_counts(normalize=True) * 100)
+        elif op == 'N'.lower():
+            print(df['Education_Level'].value_counts())
+
+    elif op == 6:
+        print('Deseja ver em porcentagem?\nS/N')
+        op = input('- ')
+        if op == 'S'.lower():
+            print(df['Purchase_Category'].value_counts(normalize= True) * 100)
+        elif op == 'N'.lower():
+            print(df['Purchase_Category'].value_counts())
+        
+
+    elif op == 7:
+        print('Deseja ver em porcentagem?\nS/N')
+        op = input('- ')
+        if op == 'S'.lower():
+            print(df['Location'].value_counts(normalize= True) * 100)
+        elif op == 'N'.lower():
+            print(df['Location'])
+    elif op == 8:
+        print(df['Time_Spent_on_Product_Research(hours)'])
+
+    elif op == 9:
+        print('Deseja ver:\n1 - todos\2 - Quantidade em especifico')
+        op = int(input('- '))
+        if op == 1:
+            print('\nAvaliação dos produtos\n')
+            for i, row in df.iterrows():
+                print(f'- Cliente id: {row['Customer_ID']} | Categoria: {row['Purchase_Category']} | Avaliação: {row['Product_Rating']}')
+        elif op == 2:
+            n = int(input('Digite a quantidade que deseja ver: '))
+            print(f'{n} top avaliações:')
+            top = df.sort_values(by='Product_Rating', ascending= False).head(n)
+            for i, row in top.iterrows():
+                print(f'{i+1}° - Cliente ID: {row['Customer_ID']} | Categoria: {row["Purchase_Category"]} | Avaliação: {row["Product_Rating"]}')
 
 def filtro_busca():
     # na opcao 2 - > inserir opcao do usuario poder escolher entre buscar por: solteiro, casado (ou outro estado social)/ genero ou tipo de renda
@@ -95,7 +138,7 @@ def filtro_busca():
             op = int(input('- '))
 
             if op == 1:
-                print('Buscar por:\n1 - ')
+                print('Buscar por:\n1 - Viúvos\n2 - Csados\n3 - Divorciados\n4 - Solteiros')
             elif op == 2:
                 print(f'{df['Marital_Status'].value_counts()}')
         
